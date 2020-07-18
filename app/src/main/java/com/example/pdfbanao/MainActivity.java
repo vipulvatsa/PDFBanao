a39a04b6a9fdc9923611ab1bed98a78cbf392250
Vipul <vatvipul@amazon.com>
2020-07-18 16:30:28 +0530

    Code changes to store images in different formats

---
+++ app/src/main/java/com/example/pdfbanao/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.pdfbanao;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.scanlibrary.ScanActivity;
+import com.scanlibrary.ScanConstants;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+//    public void openCamera(View view) {
+//        int REQUEST_CODE = 99;
+//        int preference = ScanConstants.OPEN_CAMERA;
+//        Intent intent = new Intent(this, ScanActivity.class);
+//        intent.putExtra(ScanConstants.OPEN_INTENT_PREFERENCE, preference);
+//        startActivityForResult(intent, REQUEST_CODE);
+//    }
+
+    public void openGallery(View view) {
+        int REQUEST_CODE = 99;
+        int preference = ScanConstants.OPEN_MEDIA;
+        Intent intent = new Intent(this, ScanActivity.class);
+        intent.putExtra(ScanConstants.OPEN_INTENT_PREFERENCE, preference);
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 99 && resultCode == Activity.RESULT_OK) {
+            Uri uri = data.getExtras().getParcelable(ScanConstants.SCANNED_RESULT);
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+//                getContentResolver().delete(uri, null, null);
+//                scannedImageView.setImageBitmap(bitmap);
+                Toast.makeText(this, "Image Saved!", Toast.LENGTH_LONG).show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
